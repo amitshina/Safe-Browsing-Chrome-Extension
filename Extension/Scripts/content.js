259a26c8b201197ffa429de1378ebe2fa383e431
Amit Shina <amitsh1808@gmail.com>
2025-09-22 00:23:47 +0300

    Displays the current tab's URL

--- Extension/Scripts/content.js
+++ Extension/Scripts/content.js
@@ -1,9 +1,9 @@
-const currentUrl = window.location.href;
-
-const mainText = document.getElementById("main_text_extension");
-
-if (mainText) {
-    mainText.textContent = currentUrl;
-}
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentUrl = tabs[0].url;
+    const mainText = document.getElementById("main_text_extension");
+    if (mainText) {
+        mainText.textContent = currentUrl;
+    }
+});
 
 console.log(currentUrl)
