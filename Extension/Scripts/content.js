86e7420f5dc2be1db506e1e6dde8a4b94349069f
Amit Shina <amitsh1808@gmail.com>
2025-09-22 00:28:00 +0300

    Change

--- Extension/Scripts/content.js
+++ Extension/Scripts/content.js
@@ -1,5 +1,6 @@
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentUrl = tabs[0].url;
+    console.log(tabs[1])
     const mainText = document.getElementById("main_text_extension");
     if (mainText) {
         mainText.textContent = currentUrl;
